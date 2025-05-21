5784a5c4524a5ee204838c692897713148ea665a
illdikko <coline.ducourtieux@gmail.com>
2025-05-21 15:49:15 +0200

    Wrap item title in a link for better navigation

--- app/view/public/catalog/catalog.php
+++ app/view/public/catalog/catalog.php
@@ -9,7 +9,7 @@ $items = $data['items'] ?? [];
         <article class="item-card">
             <img class="item-img" src="/assets/images/<?php echo $item['primary_picture']; ?>"
                 alt="<?php echo $item['label']; ?>">
-            <h2 class="item-card-title"><?php echo $item['label']; ?></h2>
+            <a href="<?php echo $item['slug']; ?>"><h2 class="item-card-title"><?php echo $item['label']; ?></h2></a>
             <p class="item-description"><?php echo $item['description']; ?></p>
             <p class="item-price"><?php echo number_format((float) $item['prix'], 2, ',', ' '); ?> €</p>
         </article>
